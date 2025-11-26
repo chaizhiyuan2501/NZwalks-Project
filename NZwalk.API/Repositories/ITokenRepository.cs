9e2913a4556186a45d81f65620dda9a188ff5d80
kirisame19 <chaizhiyuan2501@gmail.com>
2025-11-26 14:20:43 +0900

    9.Create JWT Token in ASP.NET Core Web API

---
+++ NZwalk.API/Repositories/ITokenRepository.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace NZwalk.API.Repositories
+{
+    public interface ITokenRepository
+    {
+        string CreateJWTToken(IdentityUser user, List<string> roles);
+    }
+}
